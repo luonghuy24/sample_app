59ae38e5aa443593973122ada6482dd9e9be202c
harry <mrluonghuy@gmail.com>
2014-02-15 09:54:56 +0700

    Finish chapter 7, user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
 SampleApp::Application.routes.draw do
-  get "user/new"
-  get "users/new"
+  resources :users
   root 'static_pages#home'
   match '/home', to: 'static_pages#home', via: 'get'
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
-  match '/signup', to: 'users#new', via: 'get'
+  match '/signup',  to: 'users#new',            via: 'get'
 
 # You can have the root of your site routed with "root"
 # root 'welcome#index'
---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name     "Trung Nguyen"
+    email    "mrTrung@amail.com"
+    password "123456"
+    password_confirmation "123456"
+  end
+end
