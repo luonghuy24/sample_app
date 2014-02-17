0f276e23cf4c618f1ed15f73421709daeea9cbb4
harry <mrluonghuy@gmail.com>
2014-02-17 12:40:07 +0700

    pass 9.3

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
   before_action :signed_in_user, only: [:edit, :update]
   before_action :correct_user,   only: [:edit, :update]
 
+
   def show
     @user = User.find(params[:id])
   end
@@ -19,6 +20,10 @@ class UsersController < ApplicationController
     else
       render 'new'
     end
+  end
+
+  def index
+    @users = User.all.paginate(page: params[:page])
   end
 
   def edit
--- spec/factories.rb
+++ spec/factories.rb
@@ -1,8 +1,8 @@
 FactoryGirl.define do
   factory :user do
-    name     "Trung Nguyen"
-    email    "mrTrung@amail.com"
-    password "123456"
-    password_confirmation "123456"
+    sequence(:name)  { |n| "Person #{n}" }
+    sequence(:email) { |n| "person_#{n}@example.com"}
+    password "foobar"
+    password_confirmation "foobar"
   end
 end
